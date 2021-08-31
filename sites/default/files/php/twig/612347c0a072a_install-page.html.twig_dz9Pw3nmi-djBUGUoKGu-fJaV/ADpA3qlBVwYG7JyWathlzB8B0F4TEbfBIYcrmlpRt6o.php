5d47ffb8a284ab336bfc86c0d0e93485f5130ccf
vickychoudhari <vishalkangra786@gmail.com>
2021-08-31 12:08:35 +0530

    add drupal 8

---
+++ sites/default/files/php/twig/612347c0a072a_install-page.html.twig_dz9Pw3nmi-djBUGUoKGu-fJaV/ADpA3qlBVwYG7JyWathlzB8B0F4TEbfBIYcrmlpRt6o.php
@@ -0,0 +1,151 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* core/themes/seven/templates/install-page.html.twig */
+class __TwigTemplate_9085c35e9e7a90163eea0742f0358de2d17fcc9125de0f5deabeeaf2d792b995 extends \Twig\Template
+{
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = ["if" => 15];
+        $filters = [];
+        $functions = [];
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['if'],
+                [],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        // line 12
+        echo "<div class=\"layout-container\">
+
+  <header role=\"banner\">";
+        // line 15
+        if (($context["site_name"] ?? null)) {
+            // line 16
+            echo "      <h1 class=\"page-title\">";
+            // line 17
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["site_name"] ?? null)), "html", null, true);
+            // line 18
+            if (($context["site_version"] ?? null)) {
+                // line 19
+                echo "          <span class=\"site-version\">";
+                echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["site_version"] ?? null)), "html", null, true);
+                echo "</span>";
+            }
+            // line 21
+            echo "      </h1>";
+        }
+        // line 23
+        echo "  </header>";
+        // line 25
+        if ($this->getAttribute(($context["page"] ?? null), "sidebar_first", [])) {
+            // line 26
+            echo "    <aside class=\"layout-sidebar-first\" role=\"complementary\">";
+            // line 27
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["page"] ?? null), "sidebar_first", [])), "html", null, true);
+            echo "
+    </aside>";
+        }
+        // line 30
+        echo "
+  <main role=\"main\">";
+        // line 32
+        if (($context["title"] ?? null)) {
+            // line 33
+            echo "      <h2 class=\"heading-a\">";
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["title"] ?? null)), "html", null, true);
+            echo "</h2>";
+        }
+        // line 35
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["page"] ?? null), "highlighted", [])), "html", null, true);
+        // line 36
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["page"] ?? null), "content", [])), "html", null, true);
+        echo "
+  </main>";
+        // line 39
+        if ($this->getAttribute(($context["page"] ?? null), "sidebar_second", [])) {
+            // line 40
+            echo "    <aside class=\"layout-sidebar-second\" role=\"complementary\">";
+            // line 41
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["page"] ?? null), "sidebar_second", [])), "html", null, true);
+            echo "
+    </aside>";
+        }
+        // line 45
+        if ($this->getAttribute(($context["page"] ?? null), "page_bottom", [])) {
+            // line 46
+            echo "    <footer role=\"contentinfo\">";
+            // line 47
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["page"] ?? null), "page_bottom", [])), "html", null, true);
+            echo "
+    </footer>";
+        }
+        // line 50
+        echo "
+</div>";
+    }
+
+    public function getTemplateName()
+    {
+        return "core/themes/seven/templates/install-page.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  120 => 50,  115 => 47,  113 => 46,  111 => 45,  106 => 41,  104 => 40,  102 => 39,  98 => 36,  96 => 35,  91 => 33,  89 => 32,  86 => 30,  81 => 27,  79 => 26,  77 => 25,  75 => 23,  72 => 21,  67 => 19,  65 => 18,  63 => 17,  61 => 16,  59 => 15,  55 => 12,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "core/themes/seven/templates/install-page.html.twig", "/app/core/themes/seven/templates/install-page.html.twig");
+    }
+}
---
+++ sites/default/files/php/twig/612347c0a072a_status-messages.html.twig_LxdnylYhPsPWu3Q0bhUd9Kb5q/vBp7DRzVHstQ2NKAafbNIVd1p4fTICsApgaTMomY55c.php
@@ -0,0 +1,145 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* core/themes/classy/templates/misc/status-messages.html.twig */
+class __TwigTemplate_ba768e2c4bb7ceef128993e69099d4220cfce6f135e8ab6ff981d374bf16f4ac extends \Twig\Template
+{
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = [
+            'messages' => [$this, 'block_messages'],
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = ["block" => 22, "for" => 23, "set" => 25, "if" => 31];
+        $filters = ["without" => 30, "length" => 37, "first" => 44];
+        $functions = [];
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['block', 'for', 'set', 'if'],
+                ['without', 'length', 'first'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        // line 22
+        $this->displayBlock('messages', $context, $blocks);
+    }
+
+    public function block_messages($context, array $blocks = [])
+    {
+        // line 23
+        $context['_parent'] = $context;
+        $context['_seq'] = twig_ensure_traversable(($context["message_list"] ?? null));
+        foreach ($context['_seq'] as $context["type"] => $context["messages"]) {
+            // line 25
+            $context["classes"] = [0 => "messages", 1 => ("messages--" . $this->sandbox->ensureToStringAllowed(            // line 27
+$context["type"]))];
+            // line 30
+            echo "  <div role=\"contentinfo\" aria-label=\"";
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["status_headings"] ?? null), $context["type"], [], "array")), "html", null, true);
+            echo "\"";
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, twig_without($this->sandbox->ensureToStringAllowed($this->getAttribute(($context["attributes"] ?? null), "addClass", [0 => ($context["classes"] ?? null)], "method")), "role", "aria-label"), "html", null, true);
+            echo ">";
+            // line 31
+            if (($context["type"] == "error")) {
+                // line 32
+                echo "      <div role=\"alert\">";
+            }
+            // line 34
+            if ($this->getAttribute(($context["status_headings"] ?? null), $context["type"], [], "array")) {
+                // line 35
+                echo "        <h2 class=\"visually-hidden\">";
+                echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["status_headings"] ?? null), $context["type"], [], "array")), "html", null, true);
+                echo "</h2>";
+            }
+            // line 37
+            if ((twig_length_filter($this->env, $context["messages"]) > 1)) {
+                // line 38
+                echo "        <ul class=\"messages__list\">";
+                // line 39
+                $context['_parent'] = $context;
+                $context['_seq'] = twig_ensure_traversable($context["messages"]);
+                foreach ($context['_seq'] as $context["_key"] => $context["message"]) {
+                    // line 40
+                    echo "            <li class=\"messages__item\">";
+                    echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($context["message"]), "html", null, true);
+                    echo "</li>";
+                }
+                $_parent = $context['_parent'];
+                unset($context['_seq'], $context['_iterated'], $context['_key'], $context['message'], $context['_parent'], $context['loop']);
+                $context = array_intersect_key($context, $_parent) + $_parent;
+                // line 42
+                echo "        </ul>";
+            } else {
+                // line 44
+                echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, twig_first($this->env, $this->sandbox->ensureToStringAllowed($context["messages"])), "html", null, true);
+            }
+            // line 46
+            if (($context["type"] == "error")) {
+                // line 47
+                echo "      </div>";
+            }
+            // line 49
+            echo "  </div>";
+            // line 51
+            $context["attributes"] = $this->getAttribute(($context["attributes"] ?? null), "removeClass", [0 => ($context["classes"] ?? null)], "method");
+        }
+        $_parent = $context['_parent'];
+        unset($context['_seq'], $context['_iterated'], $context['type'], $context['messages'], $context['_parent'], $context['loop']);
+        $context = array_intersect_key($context, $_parent) + $_parent;
+    }
+
+    public function getTemplateName()
+    {
+        return "core/themes/classy/templates/misc/status-messages.html.twig";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  116 => 51,  114 => 49,  111 => 47,  109 => 46,  106 => 44,  103 => 42,  95 => 40,  91 => 39,  89 => 38,  87 => 37,  82 => 35,  80 => 34,  77 => 32,  75 => 31,  69 => 30,  67 => 27,  66 => 25,  62 => 23,  56 => 22,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "core/themes/classy/templates/misc/status-messages.html.twig", "/app/core/themes/classy/templates/misc/status-messages.html.twig");
+    }
+}
